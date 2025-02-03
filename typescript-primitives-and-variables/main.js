69ea5952035f651e6a6bca970a6cdad3aadc138b
zzse <125266206+yoshi182023@users.noreply.github.com>
2025-02-03 22:07:13 +0000

    Completed typescript-primitives-and-variables

---
+++ typescript-primitives-and-variables/main.js
@@ -0,0 +1,30 @@
+'use strict';
+// fullName - a string containing your full name.
+// Give it a type string and a string value.
+const fullName = 'Mia Gong';
+// isCool - a boolean indicating whether or not you are cool.
+// Give it a type boolean and a boolean value.
+const isCool = true;
+// totalPets - a number indicating the number of pets you live with.
+// Give it a type number and a number value.:
+const totalPets = 3;
+console.log('value of fullName:', fullName);
+console.log('value of isCool:', isCool);
+console.log('value of totalPets:', totalPets);
+console.log('typeof fullName:', typeof fullName);
+console.log('typeof isCool:', typeof isCool);
+console.log('typeof totalPets:', typeof totalPets);
+// Within main.ts, declare another variable named empty
+// and assign it the value null and type null.
+const empty = null;
+// declaring a null value variable
+// Log empty to the console.
+// Log the typeof empty to the console.
+console.log('value of empty:', empty);
+console.log('typeof empty:', typeof empty);
+// declare another variable named nothing with the type undefined,
+// but don't assign it a value.
+let nothing;
+/// / declaring a variable without assigning a value to it
+console.log('value of nothing:', nothing);
+console.log('typeof nothing:', typeof nothing);
