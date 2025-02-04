8f6f87b5727aa2967b42a54d31cd65cbf6c7f40a
zzse <125266206+yoshi182023@users.noreply.github.com>
2025-02-04 23:29:53 +0000

    typescript-arrays

---
+++ typescript-arrays/javascript-arrays-quiz-notes.md
@@ -0,0 +1,51 @@
+# javascript-arrays-quiz-notes
+
+## Quiz Questions
+
+Answer the following questions in the provided markdown file before turning in this exercise:
+
+- What are arrays used for?
+  Arrays allow you to store multiple values in a single variable.
+  This can be useful when you want to group related data together.
+  Ordering: Arrays are ordered, meaning that the items in an array have a specific order.
+  This can be useful when the order of your data matters.
+
+- Describe array literal notation.
+  const arr = [1, 2, 3, 4];
+
+- How are arrays different from "plain" objects?
+  Objects are used to store collections of key-value pairs, where keys are strings (or Symbols) and values can be any data type. Arrays are used to store ordered collections of elements, typically of the same type.
+- What number represents the first index of an array?
+
+- What is the `length` property of an array?
+  arrays are indexed starting at 0
+- How do you calculate the last index of an array?
+  subtract 1 from the length
+
+## Notes
+
+All student notes should be written here.
+
+How to write `Code Examples` in markdown
+
+for JS:
+
+```javascript
+const data = 'Howdy';
+```
+
+for HTML:
+
+```html
+<div>
+  <p>This is text content</p>
+</div>
+```
+
+for CSS:
+
+```css
+div {
+  width: 100%;
+}
+```
---
+++ typescript-arrays/main.js
@@ -0,0 +1,39 @@
+'use strict';
+// In main.ts, create an array literal containing the strings 'red', 'white', and 'blue'
+// assign it to a new variable named colors.
+const colors = ['red', 'white', 'blue'];
+// Log each value of the colors array to the console with proper labels
+console.log('Value of colors[0]:', colors[0]);
+console.log('Value of colors[1]:', colors[1]);
+console.log('Value of colors[2]:', colors[2]);
+// 3 Create a template literal which interpolates each of the values inside of the colors array,
+// and log the string to the console
+console.log(`America is ${colors[0]}, ${colors[1]}, and ${colors[2]}.`);
+// 4  Assign 'green' to index 2 of the colors array
+colors[2] = 'green';
+// 5 Create another template literal interpolating the updated values of the colors array
+console.log(`Mexico is ${colors[0]}, ${colors[1]}, and ${colors[2]}.`);
+// 6 Log the value of the colors array to the console
+console.log('value of colors:', colors);
+// 7 Log the typeof colors to the console
+console.log('typeof colors:', typeof colors);
+// Create a new array literal containing four strings and assign it to a new variable named students.
+// Each string should be a person's name.
+const students = ['Alice', 'Bob', 'Charlie', 'Mia'];
+// 2 Use dot notation to access the length property of the students array
+// and assign it to a new variable named numberOfStudents.
+const numberOfStudents = students.length;
+// 3 Log the value of numberOfStudents to the console within a string
+// like in the example log output below.
+console.log(`There are ${numberOfStudents} students in the class.`);
+// 4 Subtract 1 from numberOfStudents to get the last index
+const lastIndex = numberOfStudents - 1;
+// 5 Use lastIndex to access the last element of the students array
+const lastStudent = students[lastIndex];
+// 6 Log the value of the lastStudent variable to the console within a string
+console.log(`The last student in the array is ${lastStudent}.`);
+// Log the value of the students array to the console
+console.log('Value of students:', students);
+// Log the typeof students to the console.
+// Note that the type is object and that the name of the data type is not logged.
+console.log('typeof students:', typeof students);
