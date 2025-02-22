274c104ff2ab2a0e93810b2417a011aeba2865e3
zzse <125266206+yoshi182023@users.noreply.github.com>
2025-02-22 00:11:11 +0000

    typescript-and-json

--- typescript-and-json/main.ts
+++ typescript-and-json/main.ts
@@ -2,8 +2,12 @@
 // Give each book an isbn, a title, and an author (all string).
 // Log the Array of books and its type to the console (see typeof).
 // Remember to label each thing you log to the console.
-
-const newArray = [
+interface Book {
+  isbn: number;
+  title: string;
+  author: string;
+}
+const newArray: Book[] = [
   { isbn: 123, title: 'Mia', author: 'Gong1' },
   { isbn: 1234, title: 'Mia', author: 'Gong2' },
   { isbn: 12345, title: 'Mia', author: 'Gong3' },
@@ -15,8 +19,12 @@ console.log('typeof newArray', typeof newArray);
 // Log the result and its type to the console (see typeof).
 
 const json = JSON.stringify(newArray);
+
 console.log('typeof JSON', typeof JSON);
 console.log('json object:', json);
+
+const studentJSON = `{"id": "23", "name":"MIa"} `;
+console.log(typeof studentJSON);
 const jsonString = `{"books":
 [
   { "isbn": "123", "title": "Mia", "author": "Gong11" },
