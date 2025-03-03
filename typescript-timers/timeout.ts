cd2044032d4d67de493808866083f589295e1f19
zzse <125266206+yoshi182023@users.noreply.github.com>
2025-03-03 19:28:05 +0000

    typescript-timers

--- typescript-timers/timeout.ts
+++ typescript-timers/timeout.ts
@@ -1,5 +1,12 @@
 // Write code in timeout.ts to update the page heading from '...' to 'Hello There'
 // after two seconds have passed.
 //  Here is an example. I
-const $message = document.querySelector('#message');
-console.log($message);
+
+function updateHeading(): void {
+  const heading = document.getElementById('message');
+  if (heading) {
+    heading.textContent = 'Hello There';
+  }
+}
+
+setTimeout(updateHeading, 2000);
